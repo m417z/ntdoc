39e440610fdf9fd65c0ff224a3b524adaec70f59
Michael Maltsev <4129781+m417z@users.noreply.github.com>
2023-09-16 17:53:17 +0300

    More tooltip improvements

--- assets/ntdoc.js
+++ assets/ntdoc.js
@@ -18,6 +18,7 @@
 
         try {
             if (typeof tippy !== 'undefined') {
+                const scrollBarWidth = getScrollBarWidth();
                 tippy('[title]', {
                     content(reference) {
                         const title = reference.getAttribute('title');
@@ -25,7 +26,7 @@
                         return title;
                     },
                     theme: 'code-preview',
-                    maxWidth: 'calc(100vw - 20px)',
+                    maxWidth: `calc(100vw - ${scrollBarWidth}px)`,
                 });
             }
         } catch (e) {
@@ -173,5 +174,32 @@
         }
 
         codeElementsContainer.append(controlButtonsContainer);
+    }
+
+    // https://stackoverflow.com/a/986977
+    function getScrollBarWidth() {
+        var inner = document.createElement('p');
+        inner.style.width = "100%";
+        inner.style.height = "200px";
+
+        var outer = document.createElement('div');
+        outer.style.position = "absolute";
+        outer.style.top = "0px";
+        outer.style.left = "0px";
+        outer.style.visibility = "hidden";
+        outer.style.width = "200px";
+        outer.style.height = "150px";
+        outer.style.overflow = "hidden";
+        outer.appendChild(inner);
+
+        document.body.appendChild(outer);
+        var w1 = inner.offsetWidth;
+        outer.style.overflow = 'scroll';
+        var w2 = inner.offsetWidth;
+        if (w1 == w2) w2 = outer.clientWidth;
+
+        document.body.removeChild(outer);
+
+        return (w1 - w2);
     }
 })();
