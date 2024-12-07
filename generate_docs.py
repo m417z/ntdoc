b66328bf38561d8507bdd9b26aab4500399b9315
Michael Maltsev <4129781+m417z@users.noreply.github.com>
2024-12-07 13:52:20 +0200

    Adjust to recent phnt changes

--- generate_docs.py
+++ generate_docs.py
@@ -280,7 +280,7 @@ def get_chunk_identifiers(chunk: str) -> List[str]:
 
     assert not chunk.startswith('typedef '), chunk
 
-    if match := re.search(r'(?:NTAPI|NTAPI_INLINE)\n(\w+)\s*\(', chunk):
+    if match := re.search(r'(?:NTAPI|NTAPI_INLINE)\s+(\w+)\s*\(', chunk):
         return [match.group(1)]
 
     assert 'NTAPI' not in chunk, chunk
