af57d503bb304c0344dec1c80f5c98eb6089d5f2
Michael Maltsev <4129781+m417z@users.noreply.github.com>
2025-09-25 20:37:31 +0300

    Sync description files, update validation logic, remove empty files

--- ntdoc_gen/validator.py
+++ ntdoc_gen/validator.py
@@ -5,37 +5,38 @@ from pathlib import Path
 
 
 def validate_description_files():
-    descriptions_ids = set(p.stem for p in Path('descriptions').glob('*.md'))
-
     with Path('docs', 'ident-to-id.json').open() as f:
         docs_ids = set(json.load(f).values())
 
-    if descriptions_ids != docs_ids:
-        print('Warning: Description files and docs do not match.')
-        print('To update, run:')
-        print()
+    description_dirs = [
+        Path('descriptions'),
+        Path('descriptions', 'undocumented.ntinternals.net'),
+    ]
 
-        non_empty = set()
-        empty = set()
+    for description_dir in description_dirs:
+        descriptions_ids = set(p.stem for p in description_dir.glob('*.md'))
+        if descriptions_ids - docs_ids:
+            print('Warning: Description files and docs do not match.')
+            print(f'Description dir: {description_dir}')
+            print('To update, run:')
+            print()
 
-        for p in descriptions_ids - docs_ids:
-            size = Path('descriptions', p + '.md').stat().st_size
-            if size > 0:
-                non_empty.add(p)
-            else:
-                empty.add(p)
+            non_empty = set()
+            empty = set()
 
-        if non_empty:
-            print('# Non-empty description files (!!!):')
-            for p in sorted(non_empty):
-                print(f'rm descriptions/{p}.md')
+            for p in descriptions_ids - docs_ids:
+                size = (description_dir / (p + '.md')).stat().st_size
+                if size > 0:
+                    non_empty.add(p)
+                else:
+                    empty.add(p)
 
-        if empty:
-            print('# Empty description files:')
-            for p in sorted(empty):
-                print(f'rm descriptions/{p}.md')
+            if non_empty:
+                print('# Non-empty description files (!!!):')
+                for p in sorted(non_empty):
+                    print(f'rm {description_dir / (p + '.md')}')
 
-        if docs_ids - descriptions_ids:
-            print('# Missing description files:')
-            for p in sorted(docs_ids - descriptions_ids):
-                print(f'touch descriptions/{p}.md')
+            if empty:
+                print('# Empty description files:')
+                for p in sorted(empty):
+                    print(f'rm {description_dir / (p + '.md')}')
