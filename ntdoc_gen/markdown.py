2aab9b50fa0134ec1122032f4c23417b4cba49c4
Michael Maltsev <4129781+m417z@users.noreply.github.com>
2025-11-10 04:44:29 +0200

    Remove escaped symbols workaround

--- ntdoc_gen/markdown.py
+++ ntdoc_gen/markdown.py
@@ -22,41 +22,6 @@ def markdown_to_html(text: str, header_ids=True, code_friendly=False) -> str:
     if header_ids:
         extras['header-ids'] = None
 
-    # Encode some escaped symbols as markdown2 doesn't handle them correctly.
-    def bracket_escape(m: re.Match) -> str:
-        # Skip if inside of a code block.
-        code_block_count = text.count('```', 0, m.start())
-        if code_block_count % 2 == 1:
-            return m.group(0)
-
-        # Skip if line inside of an inline code span.
-        line_start = text.rfind('\n', 0, m.start()) + 1
-        backtick_count = text.count('`', line_start, m.start())
-        if backtick_count % 2 == 1:
-            return m.group(0)
-
-        # Skip if escaped with an odd number of backslashes.
-        preceding_text = text[:m.start()]
-        trailing_backslashes = 0
-        for char in reversed(preceding_text):
-            if char == '\\':
-                trailing_backslashes += 1
-            else:
-                break
-        if trailing_backslashes % 2 == 1:
-            return m.group(0)
-
-        symbol = m.group(1)
-        if symbol == '<':
-            return '&lt;'
-        elif symbol == '>':
-            return '&gt;'
-        else:
-            assert symbol == '&'
-            return '&amp;'
-
-    text = re.sub(r'\\(?!<br>)([<>&])', bracket_escape, text)
-
     # A workaround for fenced code blocks not being separated from lists in safe mode.
     text = re.sub(r'^```(.*?)```', r'<!-- CODE_MARKER -->\n\g<0>', text, flags=re.MULTILINE | re.DOTALL)
 
