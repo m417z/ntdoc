0a4c188290d9386c6a0e27643d63508e6a3e9698
Michael Maltsev <4129781+m417z@users.noreply.github.com>
2025-12-22 00:49:06 +0200

    cmarkgfm: open links in new tabs

--- ntdoc_gen/markdown.py
+++ ntdoc_gen/markdown.py
@@ -1,14 +1,21 @@
 """Markdown processing utilities."""
 
+import re
+
 import cmarkgfm
 from cmarkgfm.cmark import Options as cmarkgfmOptions
 
 
 def markdown_to_html(text: str, header_ids=True, code_friendly=False) -> str:
-    return cmarkgfm.markdown_to_html_with_extensions(
+    html: str = cmarkgfm.markdown_to_html_with_extensions(
         text,
         options=cmarkgfmOptions.CMARK_OPT_GITHUB_PRE_LANG,
         extensions=[
             'table',
         ],
     )
+
+    # Make links open in new tabs.
+    html = re.sub(r'<a\b', r'<a rel="noopener" target="_blank"', html)
+
+    return html
