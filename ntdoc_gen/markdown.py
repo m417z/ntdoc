ee1f68b3b522fad6594e55ebac3adfc52b7a6cd3
Michael Maltsev <4129781+m417z@users.noreply.github.com>
2025-12-22 00:35:16 +0200

    Switch to cmarkgfm

--- ntdoc_gen/markdown.py
+++ ntdoc_gen/markdown.py
@@ -1,31 +1,14 @@
 """Markdown processing utilities."""
 
-import re
-
-import markdown2
+import cmarkgfm
+from cmarkgfm.cmark import Options as cmarkgfmOptions
 
 
 def markdown_to_html(text: str, header_ids=True, code_friendly=False) -> str:
-    extras = {
-        'breaks': {'on_backslash': True},
-        'cuddled-lists': None,
-        'fenced-code-blocks': None,
-        'tables': None,
-        'target-blank-links': None,
-    }
-
-    if code_friendly:
-        extras['code-friendly'] = None
-    else:
-        extras['middle-word-em'] = {'allowed': False}
-
-    if header_ids:
-        extras['header-ids'] = None
-
-    html = markdown2.markdown(text, extras=extras, safe_mode='escape')
-
-    # Undo safe mode for <br> and comments.
-    html = html.replace('&lt;br&gt;', '<br>')
-    html = re.sub(r'&lt;!--(.*?)--&gt;', r'<!--\1-->', html, flags=re.DOTALL)
-
-    return html
+    return cmarkgfm.markdown_to_html_with_extensions(
+        text,
+        options=cmarkgfmOptions.CMARK_OPT_GITHUB_PRE_LANG,
+        extensions=[
+            'table',
+        ],
+    )
