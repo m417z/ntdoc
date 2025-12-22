9c1bb6408fc17ca1be0fc956324db07222849128
Michael Maltsev <4129781+m417z@users.noreply.github.com>
2025-12-23 00:40:09 +0200

    Remove temporary markdown code

--- ntdoc_gen/markdown.py
+++ ntdoc_gen/markdown.py
@@ -25,10 +25,4 @@ def markdown_to_html(text: str, header_ids=True, code_friendly=False) -> str:
     # Restore <br /> tags.
     html = html.replace(BR_SENTINEL, '<br />')
 
-    # Remove code class attributes for now. TODO: remove.
-    html = re.sub(r'<pre><code( class="[^"]+")?>\s*', '<pre><code>', html)
-
-    # Temporary, TODO: remove.
-    html = re.sub(r'<li>(.*?)</li>', r'<li>\n\1\n</li>', html)
-
     return html
