86e04041efb24dc735694cb9f45cac523e8ad68b
Michael Maltsev <4129781+m417z@users.noreply.github.com>
2025-12-22 21:21:28 +0200

    Markdown experiment: Use cmarkgfm

--- ntdoc_gen/markdown.py
+++ ntdoc_gen/markdown.py
@@ -1,56 +1,34 @@
 """Markdown processing utilities."""
 
 import re
+import secrets
 
-import markdown2
+import cmarkgfm
+
+BR_SENTINEL = secrets.token_hex(8)
 
 
 def markdown_to_html(text: str, header_ids=True, code_friendly=False) -> str:
-    extras = {
-        'breaks': {'on_backslash': True},
-        'cuddled-lists': None,
-        'fenced-code-blocks': None,
-        'tables': None,
-        'target-blank-links': None,
-    }
+    # Preserve <br /> tags.
+    text = re.sub(r'<br\s*/?>', BR_SENTINEL, text)
 
-    if code_friendly:
-        extras['code-friendly'] = None
-    else:
-        extras['middle-word-em'] = {'allowed': False}
+    html: str = cmarkgfm.markdown_to_html_with_extensions(
+        text,
+        extensions=[
+            'table',
+        ],
+    )
 
-    # if header_ids:
-    #     extras['header-ids'] = None
+    # Make links open in new tabs.
+    html = re.sub(r'<a\b', r'<a rel="noopener" target="_blank"', html)
 
-    html = markdown2.markdown(text, extras=extras, safe_mode='escape')
+    # Restore <br /> tags.
+    html = html.replace(BR_SENTINEL, '<br />')
 
-    # Undo safe mode for <br> and comments.
-    html = html.replace('&lt;br&gt;', '<br>')
-    html = re.sub(r'&lt;!--(.*?)--&gt;', r'<!--\1-->', html, flags=re.DOTALL)
+    # Remove code class attributes for now. TODO: remove.
+    html = re.sub(r'<pre><code( class="[^"]+")?>\s*', '<pre><code>', html)
 
-    # Fix a specific common quirk.
-    html = re.sub(r'<strong>(\w+)\*(\w+)</strong>\*', r'<strong>\1<em>\2</em></strong>', html)
-
-    # Remove leading whitespace.
-    html = re.sub(r'(?m)^[ \t]+', '', html)
-
+    # Temporary, TODO: remove.
     html = re.sub(r'<li>(.*?)</li>', r'<li>\n\1\n</li>', html)
-
-    # Normalize self-closing tags.
-    html = re.sub(r'<br\s*/?>', '<br />', html)
-    html = re.sub(r'<hr\s*/?>', '<hr />', html)
-    html = re.sub(r'<img([^>]*)>', r'<img\1 />', html)
-
-    # Remove multiple newlines outside of <pre> blocks.
-    def newlines_sub(match: re.Match) -> str:
-        start_index = match.start()
-        pre_open_count = html.count('<pre', 0, start_index)
-        pre_end_count = html.count('</pre>', 0, start_index)
-        if pre_open_count > pre_end_count:
-            # Inside a pre block.
-            return match.group(0)
-        return f'\n'
-
-    html = re.sub(r'\n{2,}', newlines_sub, html)
 
     return html
