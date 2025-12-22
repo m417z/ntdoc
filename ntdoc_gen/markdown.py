28865d56fc51a4ad489a63e74896917a3f3893aa
Michael Maltsev <4129781+m417z@users.noreply.github.com>
2025-12-22 02:17:07 +0200

    Remove bleach, cmarkgfm is probably good enough (and faster)

--- ntdoc_gen/markdown.py
+++ ntdoc_gen/markdown.py
@@ -1,63 +1,18 @@
 """Markdown processing utilities."""
 
 import re
+import secrets
 
-import bleach
 import cmarkgfm
 from cmarkgfm.cmark import Options as cmarkgfmOptions
 
-ALLOWED_ATTRIBUTES: dict[str, list[str]] = {
-    **bleach.sanitizer.ALLOWED_ATTRIBUTES,
-    'a': [
-        *bleach.sanitizer.ALLOWED_ATTRIBUTES.get('a', []),
-        'rel',
-        'target',
-    ],
-    'pre': ['lang'],
-    'td': ['align'],
-    'th': ['align'],
-    'img': ['src', 'alt', 'title'],
-    'ol': ['start'],
-}
-
-
-def attributes_filter(tag: str, name: str, value: str) -> bool:
-    allowed_attrs = ALLOWED_ATTRIBUTES.get(tag, [])
-    if name in allowed_attrs:
-        return True
-    print(f'Warning: Disallowed attribute "{name}" on <{tag}> tag.')
-    return False
-
-
-cleaner = bleach.Cleaner(
-    tags={
-        *bleach.sanitizer.ALLOWED_TAGS,
-        'p',
-        'pre',
-        'code',
-        'span',
-        'h1',
-        'h2',
-        'h3',
-        'h4',
-        'h5',
-        'h6',
-        'table',
-        'thead',
-        'tbody',
-        'tr',
-        'th',
-        'td',
-        'hr',
-        'br',
-        'img',
-    },
-    attributes=attributes_filter,
-    strip_comments=False,
-)
+BR_SENTINEL = secrets.token_hex(8)
 
 
 def markdown_to_html(text: str, header_ids=True, code_friendly=False) -> str:
+    # Preserve <br /> tags.
+    text = re.sub(r'<br\s*/?>', BR_SENTINEL, text)
+
     html: str = cmarkgfm.markdown_to_html_with_extensions(
         text,
         options=(
@@ -71,6 +26,7 @@ def markdown_to_html(text: str, header_ids=True, code_friendly=False) -> str:
     # Make links open in new tabs.
     html = re.sub(r'<a\b', r'<a rel="noopener" target="_blank"', html)
 
-    html = cleaner.clean(html)
+    # Restore <br /> tags.
+    html = html.replace(BR_SENTINEL, '<br />')
 
     return html
